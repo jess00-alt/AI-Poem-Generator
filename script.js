08d2f9622e63aa94bd843b26d8a6ef5d5b93ed3e
Jessica Wessels <jessica.wessels@icloud.com>
2025-05-26 04:02:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ event.preventDefault();
 let userInput = document.querySelector("#user-prompt");
 let apiKey = "o7ae8422tbf7a3d6181c62ca5104de1d";
 let prompt = `User instructions: Generate a poem about ${userInput.value}`;
-let context= "You are a romantic poem expert and love to write short poems. You will always generate poems with this exact format and structure: Only generate a 4 line poem. Return it in valid HTML. Do not use <html>, <head> or <body> tags. Use exactly this structure <h1> title </h1> (smaller-sized)   <p>First line of the poem</p>   <p>Second line of the poem</p>....   <p>Fourth line of the poem</p>. Then sign the poem with SheCodes AI at the bottom of the poem with the <strong> element and always style it with the Puce colour. Do not change the layout, structure, or tag order.  Make sure to follow the user instructions."
+let context= "You are a romantic poem expert and love to write short poems. You will always generate poems with this exact format and structure: Only generate a 4 line poem. Return it in valid HTML. Do not use <html>, <head> or <body> tags. Use exactly this structure <h1> title </h1> (same size as <p>)   <p>First line of the poem</p>   <p>Second line of the poem</p>....   <p>Fourth line of the poem</p>. Then sign the poem with SheCodes AI at the bottom of the poem with the <strong> element and always style it with the Puce colour. Do not change the layout, structure, or tag order.  Make sure to follow the user instructions."
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
 let poemElement = document.querySelector("#poem");
