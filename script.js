8b6b8f39d032461ea56b2ada5718b647fc11e4ac
Jessica Wessels <jessica.wessels@icloud.com>
2025-05-26 03:58:13 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-    console.log("poem generated");
     let plainText = response.data.answer;
     new Typewriter("#poem", {
         strings: plainText,
@@ -7,6 +6,10 @@ function displayPoem(response) {
         delay: 1,
         cursor: ""
     });
+
+
+
+
 
 }
 
@@ -16,12 +19,12 @@ event.preventDefault();
 let userInput = document.querySelector("#user-prompt");
 let apiKey = "o7ae8422tbf7a3d6181c62ca5104de1d";
 let prompt = `User instructions: Generate a poem about ${userInput.value}`;
-let context= "You are a romantic poem expert and love to write short poems. You will always generate poems with this exact format and structure: Only generate a 4 line poem. Return it in valid HTML. Do not use <html>, <head> or <body> tags. Use exactly this structure <h1> title </h1> (small-sized)   <p>First line of the poem</p>   <p>Second line of the poem</p>....   <p>Fourth line of the poem</p>. Then sign the poem with SheCodes AI at the bottom of the poem with the <strong> element and always style it with the Puce colour. Do not change the layout, structure, or tag order.  Make sure to follow the user instructions."
+let context= "You are a romantic poem expert and love to write short poems. You will always generate poems with this exact format and structure: Only generate a 4 line poem. Return it in valid HTML. Do not use <html>, <head> or <body> tags. Use exactly this structure <h1> title </h1> (smaller-sized)   <p>First line of the poem</p>   <p>Second line of the poem</p>....   <p>Fourth line of the poem</p>. Then sign the poem with SheCodes AI at the bottom of the poem with the <strong> element and always style it with the Puce colour. Do not change the layout, structure, or tag order.  Make sure to follow the user instructions."
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
-console.log("generating poem...");
-console.log(`Prompt: ${prompt}`);
-console.log(`Context: ${context}`);
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class = "generating"> ⏳ Generating a poem about ${userInput.value} </div>`
 
 axios.get(apiUrl).then(displayPoem);
 
